887d951ed1a18151b6830a7cb01089959880c765
Oscar Mejia <138163475+mejiaroo@users.noreply.github.com>
2026-05-06 13:06:46 -0400

    case sensitive issue

--- src/aboutus.jsx
+++ src/aboutus.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-import "./AboutUs.css";
+import "./aboutus.css";
 
 export default function AboutUs() {
   return (
